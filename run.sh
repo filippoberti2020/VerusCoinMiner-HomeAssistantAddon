350f6152ab9b2fee24c6b922acf8eeff07804558
Filippo Berti <67099611+filippoberti2020@users.noreply.github.com>
2024-04-19 16:37:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
-# Load the environment variables from the .env file
-if [ -f .env ]; then
-    export $(cat .env | xargs)
-fi
+echo "Verus Coin Miner."
+echo ""
 
-# Run the ccminer-verus binary
-ccminer-verus -a verus -o $MINING_POOL -u $WALLET_ADDRESS.$VERUS_USERNAME -p $VERUS_PASSWORD
+USERNAME=$(bashio::config 'username')
+WALLET_ADDRESS=$(bashio::config 'wallet_address')
+MINING_POOL=$(bashio::config 'mining_pool')
+VERUS_PASSWORD=$(bashio::config 'verus_password')
 
+echo "Wallet address is: " $WALLET_ADDRESS
+echo "Mining pool is: " $MINING_POOL
+echo "Username is: " $USERNAME
+echo "Password is: " $VERUS_PASSWORD
+
+echo "Run ccminer-verus . . ."
+ccminer-verus -a verus -o $MINING_POOL -u $WALLET_ADDRESS.$USERNAME -p $VERUS_PASSWORD
